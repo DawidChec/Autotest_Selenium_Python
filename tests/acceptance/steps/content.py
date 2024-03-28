84785175e2a3e3fe85c27c7b5b0ff40a3bc4f824
checd <checdd@gmail.com>
2024-03-28 10:31:36 +0100

    Created Home_Page files and updeted locators

--- tests/acceptance/steps/content.py
+++ tests/acceptance/steps/content.py
@@ -1,15 +1,15 @@
 from behave import *
-from selenium.webdriver.common.by import By
+from tests.acceptance.locators.home_page import HomePageLocators
 
 use_step_matcher('re')
 
 
 @then('There is a title shown on the page')
 def step_impl(context):
-    title_tag = context.browser.find_element(By.TAG_NAME, 'h1')
+    title_tag = context.browser.find_element(*HomePageLocators.TITLE)
     assert title_tag.is_displayed()
 
 @step('The title tag has content "(.*)"')
 def step_impl(context, content):
-    title_tag = context.browser.find_element(By.TAG_NAME, 'h1')
+    title_tag = context.browser.find_element(*HomePageLocators.TITLE)
     assert title_tag.text == content
