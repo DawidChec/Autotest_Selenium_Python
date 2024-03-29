81a354b60680723767eeb471995934dcbde53f18
checd <checdd@gmail.com>
2024-03-29 23:09:59 +0100

    adding new tests-scenario and test

--- tests/acceptance/steps/content.py
+++ tests/acceptance/steps/content.py
@@ -1,6 +1,6 @@
 from behave import *
 from tests.acceptance.page_model.base_page import BasePage
-
+from tests.acceptance.page_model.blog_page import BlogPage
 
 use_step_matcher('re')
 
@@ -15,3 +15,10 @@ def step_impl(context, content):
     page = BasePage(context.driver)
     tag = page.title
     assert tag.text == content
+
+@then('I can see there is a posts section on the page')
+def step_impl(context):
+    page = BlogPage(context.driver)
+    tag = page.posts_section
+
+    assert tag.is_displayed()
