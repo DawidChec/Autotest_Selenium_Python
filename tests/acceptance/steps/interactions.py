80752af2c6025369e0a22506305c39027bbcd83e
checd <checdd@gmail.com>
2024-03-29 23:49:35 +0100

    Creating 'User can create new posts' test

--- tests/acceptance/steps/interactions.py
+++ tests/acceptance/steps/interactions.py
@@ -3,6 +3,7 @@ import time
 from behave import *
 
 from tests.acceptance.page_model.base_page import BasePage
+from tests.acceptance.page_model.new_post_page import NewPostPage
 
 use_step_matcher('re')
 
@@ -19,3 +20,12 @@ def step_impl(context,link_text):
     else:
         raise RuntimeError()
 
+@when('I enter "(.*)" in the "(.*)" field')
+def step_impl(context, content, field_name):
+    page = NewPostPage(context.driver)
+    page.form_field(field_name).send_keys(content)
+
+@when('I press the submit button')
+def step_impl(context):
+    page = NewPostPage(context.driver)
+    page.submit_button.click()
